7f5fe2232b883cd8c2fb63fce23f8aac51eb0e2a
Franco Nogarin <spydmobile@gmail.com>
2023-02-12 12:49:49 -0700

    add work

--- convert.js
+++ convert.js
@@ -3,8 +3,19 @@ const fs   = require('fs');
 
 // Get document, or throw exception on error
 try {
-  const doc = yaml.load(fs.readFileSync('maven-publish.yml', 'utf8'));
+  const doc = yaml.load(fs.readFileSync('data/foo.yml', 'utf8'));
   console.log(doc);
 } catch (e) {
   console.log(e);
+}
+
+const steps = doc.jobs.build.steps
+
+for (const step of steps) {
+
+  // extract elements from each step.
+  const name = step.name
+  const run = step.run
+  
+  
 }
